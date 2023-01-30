de95962eebcc1e5839b5be4465c584ddcd802422
이창공 <ichang-gong@ichang-gongs-Mac-mini.local>
2023-01-30 17:00:07 +0900

    progress

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import { Octokit } from "octokit";
 import * as fs from "fs";
+import { writeFileSync, readFileSync } from 'fs';
 import { parse } from 'csv-parse';
+import { join } from 'path';
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
+var result: string = '';
+module Environment {
+    export class Sub {
+        owner: string | undefined;
+        repo: string | undefined;
+        id: number | undefined;
+        web_rul: string | undefined;
+        title: string | undefined;
+        body: string | undefined;
+    }
+}
 
 fs.createReadStream("./assets/repositories.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
@@ -14,33 +27,58 @@ fs.createReadStream("./assets/repositories.csv")
     (row: string[]) => {
         const owner = row[0];
         const repo = row[1];
-        // console.log(`owner: ${owner}, repo: ${repo}`);
         
-        // getIssues(owner, repo).then((issues) => {
-        //     issues.data.forEach((issue : any)=> {
-        //         issue.title.split(" ").forEach((word: string) => {
+        getIssues(owner, repo).then((issues) => {
+            for (var issue of issues.data) {
+                var found = false;
+                // console.log(issue);
+                for (var word of issue.title.split(" ")) {
+                    if (word.toLowerCase().includes("overflow")) {
+                        console.log(owner + "," + repo + "," + issue.id + "," + issue.url + "," + issue.title + "," + issue.body + "\n");
+                        result += owner + "," + repo + "," + issue.id + "," + issue.url + "," + issue.title + "," + issue.body + "\n";
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    for (var word of issue.body.split(" ")) {
+                        if (word.toLowerCase().includes("overflow")) {
+                            console.log(owner + "," + repo + "," + issue.id + "," + issue.url + "," + issue.title + "," + issue.body + "\n");
+                            result += owner + "," + repo + "," + issue.id + "," + issue.url + "," + issue.title + "," + issue.body + "\n";
+                            break;
+                        }
+                    }
+                }
+            }
+            // issues.data.forEach((issue : any)=> {
+            //     if (issue.title.split(" ").forEach((word: string) => { word.toLowerCase().includes("overflow") })) {
+            //         console.log(issue.owner + "," + issue.repo + "," + issue.id + "," + issue.web_url + "," + issue.title + "," + issue.body + "\n");
+            //         result += issue.owner + "," + issue.repo + "," + issue.id + "," + issue.web_url + "," + issue.title + "," + issue.body + "\n";
+            //     }
+
+                    
+                })
+        // });
+
+        
+     
+        syncWriteFile('./result.csv', result);
+        
+        // getCommits(owner, repo).then((commits) => {
+        //     console.log(commits);
+        //     commits.data.forEach((commit : any)=> {
+        //         console.log(commit.commit.message);
+        //         commit.commit.title.split(" ").forEach((word: string) => {
         //             if (word.toLowerCase().includes("overflow")) {
-        //                 console.log(`owner: ${owner}, repo: ${repo}, issue: ${issue.title}`);
+        //                 console.log(`owner: ${owner}, repo: ${repo}, commit: ${commit.commit.message}`);
         //             }
         //         })
-        // });
-        getCommits(owner, repo).then((commits) => {
-            commits.data.forEach((commit : any)=> {
-                commit.commit.title.split(" ").forEach((word: string) => {
-                    console.log(word);
-                    if (word.toLowerCase().includes("overflow")) {
-                        console.log(`owner: ${owner}, repo: ${repo}, commit: ${commit.commit.message}`);
-                    }
-                })
-            });
-        });
-    }
-    
-        );
-    
-    
-    
+        //     });
 
+        });
+    
+    
+    
   
 
 
@@ -58,6 +96,22 @@ function getIssues(owner:string, repo:string) {
     });
     
 }
+
+function syncWriteFile(filename: string, data: any) {
+    /**
+     * flags:
+     *  - w = Open file for reading and writing. File is created if not exists
+     *  - a+ = Open file for reading and appending. The file is created if not exists
+     */
+    writeFileSync(join(__dirname, filename), data, {
+      flag: 'w',
+    });
+  
+    const contents = readFileSync(join(__dirname, filename), 'utf-8');
+    console.log(contents); // 👉️ "One Two Three Four"
+  
+    return contents;
+  }
 
 // try {
 //     const { data } = await octokit.request("GET /repos/{owner}/{repo}/issues", {
